7d18c3d469d8e8f4f71763f9d40a31b75ef58d8c
Mazhar <mazhar.rajani@godspeedgames.com>
2022-03-04 14:39:28 +0530

    update

---
+++ Assets/_Main/Scripts/Key.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using TMPro;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField] private Transform keyTransform = default;
+    [SerializeField] private TMP_Text text = default;
+    [SerializeField] private float animTime = default;
+    [SerializeField] private int vibrato = default;
+    [SerializeField] private float elasticity = default;
+    [SerializeField] private float height = default;
+    [SerializeField] private float maxDelay = default;
+
+    public void Setup()
+    {
+        char c = (char)Random.Range('A', 'Z');
+        text.SetText(c.ToString());
+        Animate();
+    }
+
+    private void Animate()
+    {
+        keyTransform.DOPunchPosition(new Vector3(0, height, 0), animTime, vibrato, elasticity).SetDelay(Random.Range(0f, maxDelay)).OnComplete(Animate);
+    }
+}
---
+++ Assets/_Main/Scripts/KeyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    [SerializeField] private int rows = default;
+    [SerializeField] private int columns = default;
+    [SerializeField] private Key keyPrefab = default;
+
+    private void Awake()
+    {
+        for (int c = 0; c < columns; c++)
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                Key key = Instantiate(keyPrefab);
+                key.transform.SetParent(transform);
+                key.transform.position = new Vector3(r, 0, c);
+                key.Setup();
+            }
+        }
+    }
+}
